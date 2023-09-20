283aeb80373905bdd78611bcc6d7e1e91aff996e
LAVEN <109652025+L4VEN@users.noreply.github.com>
2023-09-20 16:48:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 const movePage = (name) => {
-  const el = document.getElementById(name);
-  if (!el) return;
-  el.scrollIntoView({ behavior: "smooth" });
+  const mp = document.getElementById(name);
+  if (!mp) return;
+  mp.scrollIntoView({ behavior: "smooth" });
 };
-
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
